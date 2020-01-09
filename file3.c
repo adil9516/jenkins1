58a8f660d4e19fd8c05f2e255d790b1a59ccbd6d
adil9516 <adil9516@gmail.com>
2020-01-09 10:18:47 -0800

    Jenkins 3

---
+++ file3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {    
+    int number1 = 2;
+   int number2 = 4;int sum;
+    
+    printf("Enter two integers: ");
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
